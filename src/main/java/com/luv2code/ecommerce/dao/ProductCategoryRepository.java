3765d917038713c7e15e22469994b25d22e3885b
haokang <haokang@uf.edu>
2023-09-19 17:55:56 -0400

    get query by id: magic

--- src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -1,6 +1,7 @@
 package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.ProductCategory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
